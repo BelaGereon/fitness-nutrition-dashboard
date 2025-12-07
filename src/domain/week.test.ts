0d61424fcd44be4905f6a9d184fce4841fdb3184
Bela Géréon <belagereon@gmx.de>
2025-12-07 15:56:10 +0100

    :test_tube: add failing test for min and max weight computation

--- src/domain/week.test.ts
+++ src/domain/week.test.ts
@@ -22,30 +22,34 @@ const makeEmptyWeek = (overrides: Partial<WeekEntry> = {}): WeekEntry => ({
   ...overrides,
 });
 
+const week: WeekEntry = makeEmptyWeek({
+  days: {
+    mon: { weightKg: 78.7, calories: 2800, proteinG: 150 },
+    tue: { weightKg: 78.0, calories: 2700, proteinG: 160 },
+    wed: {
+      /* no data */
+    },
+    thu: { weightKg: 77.8, calories: 2600, proteinG: 155 },
+    fri: { weightKg: 78.7, calories: 2900, proteinG: 170 },
+    sat: { weightKg: 78.5, calories: 3000, proteinG: 165 },
+    sun: { weightKg: 79.0, calories: 3100, proteinG: 180 },
+  },
+});
+
 describe("computeWeekMetrics", () => {
+  const metrics = computeWeekMetrics(week);
+
   it("computes the averare weight correctly", () => {
-    const week: WeekEntry = makeEmptyWeek({
-      days: {
-        mon: { weightKg: 78.7, calories: 2800, proteinG: 150 },
-        tue: { weightKg: 78.0, calories: 2700, proteinG: 160 },
-        wed: {
-          /* no data */
-        },
-        thu: { weightKg: 77.8, calories: 2600, proteinG: 155 },
-        fri: { weightKg: 78.7, calories: 2900, proteinG: 170 },
-        sat: { weightKg: 78.5, calories: 3000, proteinG: 165 },
-        sun: { weightKg: 79.0, calories: 3100, proteinG: 180 },
-      },
-    });
-
-    const metrics = computeWeekMetrics(week);
-
     expect(metrics.avgWeightKg).toBeCloseTo(
       (78.7 + 78.0 + 77.8 + 78.7 + 78.5 + 79.0) / 6
     );
   });
 
-  it.todo("computes min and max weight correctly");
+  it("computes min and max weight correctly", () => {
+    expect(metrics.minWeightKg).toBe(77.8);
+    expect(metrics.maxWeightKg).toBe(79.0);
+  });
+
   it.todo("computes average calories correctly");
   it.todo("computes average protein correctly");
   it.todo("computes average protein per kg correctly");
