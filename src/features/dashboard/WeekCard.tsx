8e21a16c7ef403d769e2ea0248b66a85dc855d0b
Bela Géréon <belagereon@gmx.de>
2025-12-15 19:04:46 +0100

    :recycle: use useState instead of custom hook

--- src/features/dashboard/WeekCard.tsx
+++ src/features/dashboard/WeekCard.tsx
@@ -22,13 +22,8 @@ export function WeekCard({ trend, base }: WeekCardProps) {
     id,
   } = trend;
 
-  const useToggle = (initialState: boolean = false) => {
-    const [isOpen, setIsOpen] = React.useState(initialState);
-    const toggle = () => setIsOpen(!isOpen);
-    return { isOpen, toggle };
-  };
-
-  const { isOpen, toggle } = useToggle();
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggle = () => setIsOpen((isOpen: boolean) => !isOpen);
 
   return (
     <li data-testid={`week-card-${id}`}>
